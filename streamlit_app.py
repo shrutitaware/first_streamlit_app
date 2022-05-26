95f15e8ca9fe9d269922f5bf3e04b057b1c3cf60
Shruti Taware <45720788+shrutitaware@users.noreply.github.com>
2022-05-26 10:04:15 -0700

    Update streamlit_app.py
    Filter by selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,14 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
 # Picker to pick multiple fruits
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+#streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
 
 #display
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
+
+fruit_selected = streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+#display
+streamlit.dataframe(fruits_to_show)
